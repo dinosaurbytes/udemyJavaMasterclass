3b70b2cf89279c314166d828708f6e439a05bcd4
Brian Liang <Brian.Liang@solium.com>
2019-03-29 07:59:32 -0600

    Coding Exercises 1-7

---
+++ src/PositiveNegativeZero.java
@@ -0,0 +1,14 @@
+public class PositiveNegativeZero {
+    public static void main(String[] args) {
+        checkNumber(-5);
+    }
+
+    public static void checkNumber(int number){
+        if(number >0){
+            System.out.println("positive");
+        }
+        else{
+            System.out.println("negative");
+        }
+    }
+}

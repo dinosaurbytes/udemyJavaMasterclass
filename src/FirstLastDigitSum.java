655288b5dcc6b6dd1dc6b884b3d37262cb772fbc
dinosaurbytes <brianliang@dinosaurbytes.com>
2019-04-11 16:00:06 -0600

    Created Coding Exercise 17

--- src/FirstLastDigitSum.java
+++ src/FirstLastDigitSum.java
@@ -22,9 +22,26 @@
 //        NOTE: Do not add a  main method to solution code.
 public class FirstLastDigitSum {
     public static void main(String[] args) {
-
+        System.out.println(sumFirstAndLastDigit(252));
+        System.out.println(sumFirstAndLastDigit(257));
+        System.out.println(sumFirstAndLastDigit(0));
+        System.out.println(sumFirstAndLastDigit(5));
+        System.out.println(sumFirstAndLastDigit(-10));
     }
     public static int sumFirstAndLastDigit(int number){
 
+        int firstNumber = 0;
+        int lastNumber = number % 10;
+
+        if (number < 0) {
+            return -1;
+        }
+
+        while (number >= 10) {
+            number /= 10;
+        } firstNumber = number;
+
+
+        return lastNumber + firstNumber;
     }
 }
